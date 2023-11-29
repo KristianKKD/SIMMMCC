956705209c7c73da9113dc71eb0736362cf9a421
KristianKKD <kkddimitrov@gmail.com>
2023-11-29 00:12:05 +0000

    Minor fixes

--- Events.py
+++ Events.py
@@ -3,11 +3,24 @@ ARRIVAL = 1
 DEPARTURE = 2
 
 class Event:
-    type = DEPARTURE
+    type = ARRIVAL
     time = 0
     relevantCustomer = None
 
-    def __init__(self, type, time, customer):
+    def __init__(self, type, time, customer): #create an event
         self.type = type
         self.time = time
         self.relevantCustomer = customer
+
+def InsertEvent(eventList, newEvent): #place an event in the relevant position in the list based on time
+    #find relevant position where the obj should be placed
+    i = 0
+    for i, event in enumerate(eventList):
+        if event.time >= newEvent.time: #new time is worse than this element
+            break #break and use the ith index (the last position)
+        elif i == len(eventList) - 1: #this is the highest time
+            i += 1 #raise the index so it gets placed at the end of the list
+
+    #insert the new event in the correct position
+    eventList.insert(i, newEvent)
+    return eventList
--- ServerCustomer.py
+++ ServerCustomer.py
@@ -5,33 +5,24 @@ NEW = 1
 HANDOVER = 2
 
 class Customer:
-    time = 0 #arrivalTime, named this to save me from having to rewrite the same function twice
     type = NEW
-    departureTime = 0 #time the customer left our system
-    serviceStartTime = 0 #time the customer began service (after waiting)
-    waitedTime = 0 #time between customer arrival and service
-    serviceTime = 0 #time the customer was using our service
-    timeInServer = 0 #total time in our servers
 
-    def __init__(self, arrivalTime, customerType=NEW):
-        self.time = arrivalTime
+    def __init__(self, customerType=NEW):
         self.type = customerType
 
-    def ServeCustomer(self, startServiceTime, serviceRate):
-        self.waitedTime = startServiceTime - self.time
-        self.serviceStartTime = startServiceTime
-        self.departureTime = startServiceTime + float(random.exponential(serviceRate, size=1))
-        self.serviceTime = self.departureTime - self.serviceStartTime
-        self.timeInServer = self.departureTime - self.time
-        return self.departureTime
+    def ServeCustomer(self, startServiceTime, serviceRate): #simulate service by return an event for when this customer will leave
+        return startServiceTime + float(random.exponential(serviceRate, size=1)) #departure time
 
 class Server:
     busyUntil = 0 #time this channel is busy for
-    timeSpentBusy = 0
+    timeSpentBusy = 0 #time this server has been busy for throughout runtime (for data collection)
 
-    def Serve(self, customer, time, serviceRate): #a customer has arrived and has been assigned to this server
-        customerDeparture = customer.ServeCustomer(time, serviceRate)
+    def Serve(self, customer, time, serviceRate): #a customer has arrived and has been assigned to this server, serve them
+        customerDeparture = customer.ServeCustomer(time, serviceRate) #get the departure time so we can build a departure event
+        
+        #collect stats
         self.busyUntil = customerDeparture
         self.timeSpentBusy += customerDeparture - time
+        
         return Events.Event(Events.DEPARTURE, customerDeparture, customer)
 
